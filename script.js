9c9850c0334c775d6741010cf6b13d1155e6ff5d
samuelltorres <samuelltorres145@gmail.com>
2023-09-06 21:06:42 -0300

    :tada: Commit inicial

---
+++ script.js
@@ -0,0 +1,22 @@
+const initTabNav = () => {
+	const tabmenu = document.querySelectorAll('.js-tabmenu li');
+	const tabcontent = document.querySelectorAll('.js-tabcontent section');
+
+	if (tabmenu.length && tabcontent.length) {
+		tabcontent[0].classList.add('ativo');
+
+		const activeTab = (index) => {
+			tabcontent.forEach((section) => {
+				section.classList.remove('ativo');
+			});
+			tabcontent[index].classList.add('ativo');
+		};
+
+		tabmenu.forEach((itemMenu, index) => {
+			itemMenu.addEventListener('click', () => {
+				activeTab(index);
+			});
+		});
+	}
+};
+initTabNav();
