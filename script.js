2c9ef482b299a21c122a9b77caf8cb1e103b7b32
samuelltorres <samuelltorres145@gmail.com>
2023-09-06 21:08:48 -0300

    :sparkles: feat: adicionado scroll suave nos links internos

--- script.js
+++ script.js
@@ -39,3 +39,30 @@ const initAccordion = () => {
 	}
 };
 initAccordion();
+
+const initSmoothScroll = () => {
+	const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+	function scrollToSection(e) {
+		e.preventDefault();
+		const href = e.currentTarget.getAttribute('href');
+		const section = document.querySelector(href);
+
+		section.scrollIntoView({
+			behavior: 'smooth',
+			block: 'start'
+		});
+
+		// Forma alternativa
+		// const topo = section.offsetTop;
+		// window.scrollTo({
+		// 	top: topo,
+		// 	behavior: 'smooth'
+		// });
+	}
+
+	linksInternos.forEach((link) => {
+		link.addEventListener('click', scrollToSection);
+	});
+};
+initSmoothScroll();
