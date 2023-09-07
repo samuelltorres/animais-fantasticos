930b3002cfb4fcf4c5caac4c1ad8797f703672e7
samuelltorres <samuelltorres145@gmail.com>
2023-09-06 21:08:03 -0300

    :sparkles: feat: adicionado accordion list

--- script.js
+++ script.js
@@ -20,3 +20,22 @@ const initTabNav = () => {
 	}
 };
 initTabNav();
+
+const initAccordion = () => {
+	const accordionList = document.querySelectorAll('.js-accordion dt');
+
+	if (accordionList.length) {
+		accordionList[0].classList.add('ativo');
+		accordionList[0].nextElementSibling.classList.add('ativo');
+
+		function activeAccordion() {
+			this.classList.toggle('ativo');
+			this.nextElementSibling.classList.toggle('ativo');
+		}
+
+		accordionList.forEach((item) => {
+			item.addEventListener('click', activeAccordion);
+		});
+	}
+};
+initAccordion();
